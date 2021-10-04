54e4e70069011f5f4c1ffd127ca05d7c5e26fceb
knutvalen <knut.valen@icloud.com>
2021-10-04 13:26:53 +0200

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -6,4 +6,17 @@ export class CartService {
   items: Product[] = [];
 
   constructor() {}
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
