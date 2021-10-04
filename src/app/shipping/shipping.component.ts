8b64401466c29dd466e7b308a5373a7cb25ee791
knutvalen <knut.valen@icloud.com>
2021-10-04 14:36:51 +0200

    1 file modified, 1 added

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./shipping.component.css'],
 })
 export class ShippingComponent {
-  constructor() {}
+  shippingCosts = this.cartService.getShippingPrices();
+
+  constructor(private cartService: CartService) {}
 }
